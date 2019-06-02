50fb0397259eeb7e9b571e164198ac9377f40fc8
Jaleel Blue <jaleel.blue97@gmail.com>
2019-06-01 21:15:20 -0400

    Added some needed functionality
    Created three functions one for the first menu prompt, another for the second menu prompt, and another to output the stats of the choosen team.
    
    Added another function to calculate average heigth, and to test if a users input was valid.
    
    I added additional comments to the script to explain logic.

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
+
 import constants
 
 # function declarations
-
-
 def clean_player_data(players):
     """Iterates through a given list of dictionaries and
     creates a new list of dictionaries with each dictionary containing altered data
@@ -20,7 +19,8 @@ def clean_player_data(players):
             if fields == 'name':
                 updated_player_info[fields] = player[fields]
             elif fields == 'guardians':
-                updated_player_info[fields] = player[fields].split("and")
+                individual_guardians = player[fields].split("and")
+                updated_player_info[fields] = [guardian.strip() for guardian in individual_guardians]
             elif fields == 'experience':
                 if player[fields].lower() == 'yes':
                     updated_player_info[fields] = True
@@ -53,7 +53,6 @@ def make_balanced_teams(players, teams = 3):
     distributes those players to one of three teams, each represented
     by a list. Those lists are then stored in a tuple and returned to the user."""
 
-    potential_teams = len(players) % teams
     sorted_team = sort_players_by_experience(players)
     experienced_players = sorted_team[0]
     inexperienced_players = sorted_team[1]
@@ -65,19 +64,156 @@ def make_balanced_teams(players, teams = 3):
     # Players are added to different teams based off the remainder of
     # index divided by potential_teams
     for index in range(0, len(experienced_players)):
-        if index % potential_teams > 1:
+        if index % teams > 1:
             balanced_team_3.append(experienced_players[index])
             balanced_team_3.append(inexperienced_players[index])
-        elif index % potential_teams == 1:
+        elif index % teams == 1:
             balanced_team_2.append(experienced_players[index])
             balanced_team_2.append(inexperienced_players[index])
-        elif index % potential_teams == 0:
+        elif index % teams == 0:
             balanced_team_1.append(experienced_players[index])
             balanced_team_1.append(inexperienced_players[index])
 
     return balanced_team_1, balanced_team_2, balanced_team_3
 
 
+def get_average_height(team):
+    """Returns the average height of the players on the team"""
+    total_height = 0
+    for player in team:
+        total_height += player['height']
+
+    return total_height / len(team)
+
+
+def test_input(user_input):
+    """Tests a given input and returns that input if it can turned into an int"""
+    try:
+        int(user_input)
+    except ValueError:
+        return "\nThat wasn't a valid input"
+    else:
+        return int(user_input)
+
+
+def menu_one(teams, team_names):
+    """Displays the first menu of the app"""
+    interface_heading = "\nBASKETBALL TEAM STATS TOOL\n"
+    menu = "-----Menu------"
+    user_options = """\nHere are your choices:\n 1) Display Team Stats\n 2) Quit"""
+    user_prompt = "\nEnter an option > "
+
+    display_team = """\n1) {}\n2) {}\n3) {}""".format(team_names[0],
+                                                 team_names[1], team_names[2])
+
+    using_app = True
+
+    print(interface_heading)
+    print(menu)
+    print(user_options)
+
+    user_menu_selection = input(user_prompt)
+
+    """
+    Tests the users given input and calls menu_two if the correct input was given
+    if the correct input was not given then an error message is given and 
+    menu_one is called again."""
+    try:
+        user_menu_selection = test_input(int(user_menu_selection))
+
+    except ValueError:
+        print(user_menu_selection)
+        menu_one(teams, team_names)
+
+    else:
+        if user_menu_selection != 1 and user_menu_selection != 2:
+            print("\nPlease select choose option 1) or 2)")
+            menu_one(teams, team_names)
+
+    if user_menu_selection == 1:
+        menu_two(teams, team_names)
+
+
+def menu_two(teams, team_names):
+    """Displays menu 2 and allows the user to select a
+    team and bring them to the screen that allows them to screen view the stats of that team"""
+    user_prompt = "\nEnter an option > "
+
+    display_team = """\n1) {}\n2) {}\n3) {}""".format(team_names[0],
+                                                 team_names[1], team_names[2])
+
+    print(display_team)
+    user_menu_selection = input(user_prompt)
+    try:
+        user_menu_selection2 = test_input(int(user_menu_selection))
+    except ValueError:
+        print(user_menu_selection2)
+        menu_two(teams, team_names)
+    else:
+        if user_menu_selection2 != 1 and user_menu_selection2 != 2 and user_menu_selection2 != 3:
+            print("\nPlease select choose option 1), 2) or 3)")
+            menu_two(teams, team_names)
+
+        elif user_menu_selection2 == 1:
+            display_stats(teams, team_names, 0)
+
+        elif user_menu_selection2 == 2:
+            display_stats(teams, team_names, 1)
+
+        elif user_menu_selection2 == 3:
+            display_stats(teams, team_names, 2)
+
+    menu_one(teams, team_names)
+
+
+def display_stats(teams, team_names, index):
+    team = teams[index]
+    team_name = team_names[index]
+
+    team_stats_heading = """\n\nTeam: {} Stats
+----------------------------"""
+    total_number_players = "\nTotal players: {}"
+    experienced_players = "\nexperienced players: {}"
+    inexperienced_players = "\ninexperienced players: {}"
+    names_of_players = "\nPlayers on Team:\n  {}"
+    player_guardians = "\nGuardians:\n {}"
+
+    average_height_display = "\nAverage Height: {}"
+    continue_prompt = "\nEnter Any key + Enter to continue..."
+
+    number_of_experienced_players = len([player for player in team if player['experience'] is True])
+    number_of_inexperienced_players = len([player for player in team if player['experience'] is False])
+    average_height = round(get_average_height(team))
+
+    print(team_stats_heading.format(team_name))
+    print(total_number_players.format(len(team)))
+    print(experienced_players.format(number_of_experienced_players))
+    print(inexperienced_players.format(number_of_inexperienced_players))
+
+    # every players name in the team is put in one list and joined
+    # by a ', ' then it is printed
+    names = [name['name'] for name in team]
+    print(names_of_players.format(", ".join(names)))
+
+    # every the name of every players guardian is added to one list
+    # and join by a ", " then it is printed
+    guardians = []
+    for player in team:
+        for guardian in player['guardians']:
+            guardians.append(guardian)
+
+    print(player_guardians.format(", ".join(guardians)))
+
+    # print the average height of the team
+    print(average_height_display.format(average_height))
+
+    # take an input from the user so that there can be delay between the last output
+    # and the reprinting of menu one
+    input(continue_prompt)
+    menu_one(teams, team_names)
+
+
+# Setting up the variables before the app runs
 PLAYERS = constants.PLAYERS
 TEAMS = constants.TEAMS
 
@@ -86,7 +222,7 @@ basket_ball_players = []
 basket_ball_teams = []
 
 if __name__ == "__main__":
+    # clean the player date , make balance teams, and start the program.
     basket_ball_players = clean_player_data(PLAYERS)
     balanced_teams = make_balanced_teams(basket_ball_players)
-
-    print(len(balanced_teams[0]) == len(balanced_teams[1]) == len(balanced_teams[2]))
+    menu_one(balanced_teams, TEAMS)
